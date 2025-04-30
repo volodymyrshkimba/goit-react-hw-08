fe102be7e16307ddb52a1c41d19707ad36093c68
Volodymyr Shkimba <shkimbavova@gmail.com>
2025-04-30 21:43:02 +0300

    add loager and message for auth

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -15,15 +15,32 @@ const authSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
+      .addCase(register.pending, (state) => {
+        state.isRefreshing = true;
+      })
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(register.rejected, (state) => {
+        state.isRefreshing = false;
+      })
+      .addCase(login.pending, (state) => {
+        state.isRefreshing = true;
       })
       .addCase(login.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(login.rejected, (state) => {
+        state.isRefreshing = false;
+      })
+      .addCase(logout.pending, (state) => {
+        state.isRefreshing = true;
       })
       .addCase(logout.fulfilled, (state) => {
         state.user = {
@@ -32,6 +49,10 @@ const authSlice = createSlice({
         };
         state.token = null;
         state.isLoggedIn = false;
+        state.isRefreshing = false;
+      })
+      .addCase(logout.rejected, (state) => {
+        state.isRefreshing = false;
       })
       .addCase(refreshUser.pending, (state) => {
         state.isRefreshing = true;
